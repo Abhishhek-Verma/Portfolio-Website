64cd175b8f361e25ebd1152d0d5b44fe01eb946c
Abhishek Verma <abhi.ups838@gmail.com>
2025-05-16 22:53:58 +0530

    Update ProjectsSection.tsx

--- src/components/sections/ProjectsSection.tsx
+++ src/components/sections/ProjectsSection.tsx
@@ -27,7 +27,7 @@ export default function ProjectsSection() {
    {
   title: "DevScope",
   description: "An AI-powered GitHub portfolio generator",
-  image: "/public/images/projects/DevScope.png",
+  image: "https://images.unsplash.com/photo-1675495666895-9091741bfd78?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   techs: [
     "MongoDB",
     "Express",
